6680cff41bc8bf42514232657c9dd143d63a0937
Zazazlik <zazazlikxxx@gmail.com>
2025-03-09 22:04:44 +0300

    changes from 09-03-2025

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,18 +1,65 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import android.widget.Button
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var searchInput: EditText
+    private lateinit var buttonClear: ImageButton
+    private var searchText: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val backButton: Button = findViewById(R.id.button_back)
+        val backButton: ImageButton = findViewById(R.id.button_back)
+        searchInput = findViewById(R.id.search_input)
+        buttonClear = findViewById(R.id.button_clear)
+
+        savedInstanceState?.let {
+            searchText = it.getString("SEARCH_TEXT", "")
+            searchInput.setText(searchText)
+        }
+
         backButton.setOnClickListener {
             finish()
         }
 
+        buttonClear.setOnClickListener {
+            clearSearch()
+        }
+
+        searchInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchText = s.toString()
+                buttonClear.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun clearSearch() {
+        searchInput.text.clear()
+        buttonClear.visibility = View.GONE
+        searchInput.clearFocus()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("SEARCH_TEXT", searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString("SEARCH_TEXT", "")
+        searchInput.setText(searchText)
     }
 }
