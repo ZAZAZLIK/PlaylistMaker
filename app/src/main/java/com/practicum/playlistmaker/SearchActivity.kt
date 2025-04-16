aa70be30ecb0cffa8fc66c86ab7fdade22145c24
Zazazlik <zazazlikxxx@gmail.com>
2025-04-16 17:52:34 +0300

    changes from 16-04-2025

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -29,10 +29,10 @@ data class SearchResponse(
 )
 
 data class Track(
-    val trackName: String,  // Название композиции
-    val artistName: String, // Имя исполнителя
-    val trackTimeMillis: Long,  // Продолжительность трека в миллисекундах
-    val artworkUrl100: String // Ссылка на изображение обложки
+    val trackName: String,
+    val artistName: String,
+    val trackTimeMillis: Long,
+    val artworkUrl100: String
 ) {
     fun getFormattedTrackTime(): String {
         val minutes = trackTimeMillis / 1000 / 60
